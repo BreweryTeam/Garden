fc4b19cc76e9445dc3ba79d027a9593a8ac7e842
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2026-07-13 11:03:02 +0200

    fix(fruits): Better hitbox placement (interaction entity)

--- src/main/java/dev/jsinco/brewery/garden/plant/PlacedFruitDisplays.java
+++ src/main/java/dev/jsinco/brewery/garden/plant/PlacedFruitDisplays.java
@@ -8,13 +8,14 @@ import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.entity.Bat;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Interaction;
 import org.bukkit.entity.ItemDisplay;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.util.Transformation;
+import org.bukkit.util.Vector;
 import org.joml.AxisAngle4f;
 import org.joml.Vector3f;
 
@@ -25,7 +26,7 @@ public record PlacedFruitDisplays(List<ItemDisplay> itemDisplays, Entity interac
 
     public static final NamespacedKey INTERACTION_ENTITY_DATA = Garden.key("linked_entities");
     public static final NamespacedKey OWNING_PLANT = Garden.key("owner");
-
+    public static final float ONE_OVER_SQRT_2 = (float) (1 / Math.sqrt(2));
 
     public static PlacedFruitDisplays generate(ItemStack itemSource, BlockFace relative, Block block, Key plantType, UUID owner, float placedScale) {
         Location center = block.getLocation().toCenterLocation()
@@ -50,12 +51,19 @@ public record PlacedFruitDisplays(List<ItemDisplay> itemDisplays, Entity interac
             entity.setPersistent(false);
             entity.setItemStack(itemSource);
         });
-        Entity interaction = center.getWorld().spawn(center, Bat.class, bat -> {
-            bat.setAI(false);
-            bat.setPersistent(false);
-            bat.setInvisible(true);
-            bat.setInvulnerable(true);
-            PersistentDataContainer pdc = bat.getPersistentDataContainer();
+        // Display entities will have
+        Vector diff = new Vector(
+                0,
+                -placedScale / 2,
+                0
+        );
+        Entity interaction = center.getWorld().spawn(center.add(diff), Interaction.class, entity -> {
+            entity.setNoPhysics(true);
+            entity.setPersistent(false);
+            entity.setInvulnerable(true);
+            entity.setInteractionWidth(placedScale * ONE_OVER_SQRT_2 * 0.9F);
+            entity.setInteractionHeight(placedScale * 0.9F);
+            PersistentDataContainer pdc = entity.getPersistentDataContainer();
             pdc.set(
                     INTERACTION_ENTITY_DATA,
                     PersistentDataType.LIST.listTypeFrom(PersistentDataType.BYTE_ARRAY),
@@ -79,5 +87,9 @@ public record PlacedFruitDisplays(List<ItemDisplay> itemDisplays, Entity interac
     public void remove() {
         itemDisplays.forEach(Entity::remove);
         interactionBox.remove();
+    }
+
+    private static Vector3f toVector4f(Vector vector) {
+        return new Vector3f((float) vector.getX(), (float) vector.getY(), (float) vector.getZ());
     }
 }
